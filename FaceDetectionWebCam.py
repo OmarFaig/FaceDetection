7e7595125d96f2e44a68257eeca5e6249021333f
Omar Faig Orujlu <42292086+OmarFaig@users.noreply.github.com>
2020-08-03 23:32:25 +0200

    Update FaceDetectionWebCam.py

--- FaceDetectionWebCam.py
+++ FaceDetectionWebCam.py
@@ -5,7 +5,7 @@ trained_face_data= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 webcam=cv2.VideoCapture(0)
-
+# in folgende While Schleife wird ständig(nahezu Echtzeit) die Aufnahme erkannt und in schwarz-weiss umgewandelt. Mit dem Taster "Q" wird der Programm abgebrochen
 while True :
         successful_frame_read,frame=webcam.read()
         grayscaled_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
